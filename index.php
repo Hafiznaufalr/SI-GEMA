47353dd913272f267b985626b9ad6c6e8ef559d1
Hafiznaufalr <hafiz.nr5758@gmail.com>
2018-06-13 00:17:09 +0700

    implementasi Content API

--- index.php
+++ index.php
@@ -125,5 +125,32 @@ $app->get('index.php/profile', function($req, $res) use ($bot)
     return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
 });
  
+$bot->getMessageContent(messageId);
+if(
+    $event['message']['type'] == 'image' or
+    $event['message']['type'] == 'video' or
+    $event['message']['type'] == 'audio' or
+    $event['message']['type'] == 'file'
+){
+    $basePath  = $request->getUri()->getBaseUrl();
+    $contentURL  = $basePath."/content/".$event['message']['id'];
+    $contentType = ucfirst($event['message']['type']);
+    $result = $bot->replyText($event['replyToken'],
+        $contentType. " yang Anda kirim bisa diakses dari link:\n " . $contentURL);
+ 
+    return $res->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
+}
 
+$app->get('/content/{messageId}', function($req, $res) use ($bot)
+{
+    // get message content
+    $route      = $req->getAttribute('route');
+    $messageId = $route->getArgument('messageId');
+    $result = $bot->getMessageContent($messageId);
+ 
+    // set response
+    $res->write($result->getRawBody());
+ 
+    return $res->withHeader('Content-Type', $result->getHeader('Content-Type'));
+});
 $app->run();
